747fccaf0fa73b3256bb55bc3f058930b08e9015
Jens Stegemann <jwstegemann@gmail.com>
2024-05-06 11:04:59 +0200

    test

--- IPAdapterPlus.py
+++ IPAdapterPlus.py
@@ -894,7 +894,8 @@ class IPAdapterLoadFaceId:
     def load(self, faceid):
         input_dir = folder_paths.get_input_directory()
         path = os.path.join(input_dir, faceid)
-        #return (torch.load(path))
+        faceId = (torch.load(path))
+        print(faceId)
         return "Hugo"
 
 
