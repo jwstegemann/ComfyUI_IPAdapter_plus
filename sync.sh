d8f19a1cbddcb2123b2a3dc26917c315629639d9
Jens Stegemann <jwstegemann@gmail.com>
2024-10-30 16:29:02 +0100

    env

--- sync.sh
+++ sync.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
+export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=$S3_SECRET_KEY
+export AWS_DEFAULT_REGION=$S3_REGION
+
 aws s3 sync . s3://yct-backup/backup_2024_10_24/ComfyUI/custom_nodes/ComfyUI_IPAdapter_plus/ --delete --exclude ".git/*"
